26176b0ab35e67ef7472f35297e5672c0d0f232a
lk543 <lk543@cornell.edu>
2022-10-20 15:15:36 -0400

    Implement 1D Movement Functionality
    - Completed a 1D array functionality
    - Pieces can be moved to specific spaces, and it would be removed from its current location

--- src/board.ml
+++ src/board.ml
@@ -43,18 +43,33 @@ let board_of_spaces = Array.make 8 (Array.make 8 space)
 let what_piece board row_index column_index =
   Array.get (Array.get board column_index) row_index
 
+let what_piece_mvp board index =
+  Array.get board index
+
 (**not done, currently makes the row empty*)
 let remove_piece board (row_index : int) (column_index : int) =
   let new_row = Array.copy (Array.get board_of_spaces column_index) in
   Array.set board column_index new_row
+
+let remove_piece_mvp board (index : int) =
+  Array.set board index space
 
 (** not done, currently replaces row with row of the pieces*)
 let place_piece board (piece : piece) (row_index : int) (column_index : int) =
   let new_row = Array.make 8 piece in
   Array.set board column_index new_row
 
+let place_piece_mvp board (piece : piece) (index : int) =
+  Array.set board index piece
+
 let move board (piece_row : int) (piece_column : int) (destination_row : int)
     (destination_column : int) =
   let piece = what_piece board piece_row piece_column in
   remove_piece board piece_row piece_column;
   place_piece board piece destination_row destination_column
+
+let move_mvp board (piece_index : int) (destination_index : int) = 
+  let piece = what_piece_mvp board piece_index in
+    remove_piece_mvp board piece_index;
+    place_piece_mvp board piece destination_index;
+
