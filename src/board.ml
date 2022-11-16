d9c030c120b48d01a1e31a8b200c211d8757d1ac
Rachael Close <rlc355@cornell.edu>
2022-11-16 18:09:39 -0500

    Updated is_under_attack function

--- src/board.ml
+++ src/board.ml
@@ -254,11 +254,17 @@ let general_moves_pawn x y =
 
 (** [general_moves_king x y] is an array of coordinates of general moves by a
     king in coordinate (x,y)*)
-    let general_moves_king x y =
-      [| (x, y + 1); (x, y - 1); (x + 1, y); (x - 1, y);  (x + 1, y + 1); 
-      (x - 1, y - 1);  (x + 1, y - 1);  (x - 1, y + 1) |]
-
-
+let general_moves_king x y =
+  [|
+    (x, y + 1);
+    (x, y - 1);
+    (x + 1, y);
+    (x - 1, y);
+    (x + 1, y + 1);
+    (x - 1, y - 1);
+    (x + 1, y - 1);
+    (x - 1, y + 1);
+  |]
 
 (** [legal_moves_knight x y] is an array of coordinates of legal moves by a
     knight in coordinate (x,y) *)
@@ -297,7 +303,7 @@ let legal_moves_queen x y =
 
 (** [legal_moves_king x y] is an array of coordinates of legal moves by a king
     in coordinate (x,y) *)
-    let legal_moves_king x y = general_moves_king x y |> moves_except_outside
+let legal_moves_king x y = general_moves_king x y |> moves_except_outside
 
 (** [is_under_attack board x y] is true if the piece in coordinate (x,y) is
     under attack in board [board] meaning it is in one of the legal moves for an
@@ -335,11 +341,11 @@ let is_under_attack board x y =
             !acc
             || s <> side_of_piece_being_checked
                && Array.mem (x, y) (legal_moves_queen i j)
-      | King, s -> 
-        acc :=
-      !acc
-      || s <> side_of_piece_being_checked
-         && Array.mem (x, y) (legal_moves_king i j)
+      | King, s ->
+          acc :=
+            !acc
+            || s <> side_of_piece_being_checked
+               && Array.mem (x, y) (legal_moves_king i j)
     done
   done;
   !acc
